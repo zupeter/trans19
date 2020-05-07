7e8f5aa033ab04c8920b93079442c999cf0ba8ea
juneluk <64723122+juneluk@users.noreply.github.com>
2020-05-08 00:59:23 +0800

    updated detail url pattern

--- src/record/location_urls.py
+++ src/record/location_urls.py
@@ -11,7 +11,7 @@ from .location_views import (
 urlpatterns = [
 	path('',location_record_list_view),
 	path('create/',location_record_create_view),
-	path('<int:location_num>/',location_record_detail_view),
+	path('<int:location_num>/detail/',location_record_detail_view),
 	path('<int:location_num>/modify/',location_record_update_view),
 	path('<int:location_num>/delete/',location_record_delete_view),
 ]
