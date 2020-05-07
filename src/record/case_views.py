824462d7cc62b2f81b8ea1d308b426fb20a85581
mansoncode <63484433+mansoncode@users.noreply.github.com>
2020-05-08 07:56:01 +0800

    update create case

--- src/record/case_views.py
+++ src/record/case_views.py
@@ -25,6 +25,7 @@ def case_record_create_view(request):
 	#ADD case(s)
 	#with a form
 	# return HttpResponse("<h1>create cases</h1>")
+	print(request.POST)
 	form = CaseForm(request.POST or None)
 	if form.is_valid():
 		print(form.cleaned_data)
@@ -45,7 +46,10 @@ def case_record_detail_view(request,case_num):
 
 def case_record_update_view(request,case_num):
 	#MODIFY case
-	return HttpResponse("<h1>update case</h1>")
+	obj_case = get_object_or_404(CaseRecord, case_number = case_num)
+	template_name = "case/modify/modify.html"
+	context = {'object_case': obj_case}
+	return render(request, template_name, context)
 
 
 def case_record_delete_view(request,case_num):
