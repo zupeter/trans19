2eef62974ce5510a0c39cc3560c8e403e1bb3178
BrokenPeter <peter720@connect.hku.hk>
2020-05-10 09:54:35 +0800

    connection

--- src/record/case_views.py
+++ src/record/case_views.py
@@ -39,7 +39,7 @@ def case_record_create_view(request):
 def case_record_detail_view(request, pkey):
 	#VIEW case
 	#get 1 object -> detail view
-	#under case details, list all the visits to locaiton
+	#under case details, list all the visits to location
 	obj_case = get_object_or_404(CaseRecord, pk=pkey)
 	template_name = "case/detail.html"
 	context = {'object_case': obj_case}
@@ -111,8 +111,17 @@ def case_record_delete_visit_view(request, pkey, vpkey):
 def case_search_connections(request, pkey, vpkey):
 	obj_case = get_object_or_404(CaseRecord, pk=pkey)
 	obj_visit = get_object_or_404(VisitRecord, pk=vpkey)
-	connection_visit = VisitRecord.objects.filter(~Q(date_from > obj_visit.date_to +2), ~Q(date_from < obj_visit.date_from -2),Q(location = obj_visit.location))
-	connection_case = get_object_or_404(CaseRecord, case_number = connection_visit.case) #someone help me link them together
-	context = {'og_case':obj_case,'og_visit':obj_visit, 'connection_case':connection_case, 'connection_visit':connection_visit}
+	# connection_visit = VisitRecord.objects.filter(~Q(date_from > obj_visit.date_to +2), ~Q(date_from < obj_visit.date_from -2),Q(location = obj_visit.location))
+	# connection_case = get_object_or_404(CaseRecord, case_number = connection_visit.case) #someone help me link them together
+	# context = {'og_case':obj_case,'og_visit':obj_visit, 'connection_case':connection_case, 'connection_visit':connection_visit}
+	form = SearchForm(request.POST or None)
+	connections = []
+	if form.is_valid():
+		print (form.cleaned_data['window'])
+		print (obj_visit.location)
+		print (obj_visit.location.visitrecord_set.all())
+		for visit in obj_visit.location.visitrecord_set.all():
+			print (visit)
+	context = {'selected_case':obj_case,'selected_visit':obj_visit, 'form':form, 'conn':connections}
 	template_name = "case/connections.html"
 	return render(request, template_name, context)
--- src/record/form.py
+++ src/record/form.py
@@ -1,20 +1,24 @@
-from django.forms import ModelForm
+from django import forms
 from .models import *
 
-class CaseForm(ModelForm):
+class CaseForm(forms.ModelForm):
 	class Meta:
 		model = CaseRecord
 		fields = ['name','id_number','date_of_birth','date_of_comfirmed','case_number']
 
 
-class LocationForm(ModelForm):
+class LocationForm(forms.ModelForm):
 	class Meta:
 		model = LocationRecord
 		fields = ['location','address','district','x_coord','y_coord']
 		#idk how to make this form, this class looks very different from the tutorial you sent us#
 
 
-class VisitForm(ModelForm):
+class VisitForm(forms.ModelForm):
 	class Meta:
 		model = VisitRecord
 		fields = ['location','date_from','date_to','case','detail','category']
+
+
+class SearchForm(forms.Form):
+	window = forms.IntegerField(label='Search Window of time')
