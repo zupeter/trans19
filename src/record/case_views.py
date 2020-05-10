726c6d88c4500f331bc2a1a692ee3c8bb4e2dca3
BrokenPeter <peter720@connect.hku.hk>
2020-05-10 11:28:38 +0800

    completed connection search html

--- src/record/case_views.py
+++ src/record/case_views.py
@@ -117,6 +117,8 @@ def case_search_connections(request, pkey, vpkey):
 	# context = {'og_case':obj_case,'og_visit':obj_visit, 'connection_case':connection_case, 'connection_visit':connection_visit}
 	form = SearchForm(request.POST or None)
 	connections = []
+	template_name = "case/connections.html"
+	context = {'selected_case':obj_case,'selected_visit':obj_visit, 'form':form}
 	if form.is_valid():
 		print (form.cleaned_data['window'])
 		print (obj_visit.location)
@@ -128,16 +130,10 @@ def case_search_connections(request, pkey, vpkey):
 			if visit.case.pk is not obj_case.pk:
 				print ("This visit in same location but not same case\n",visit)
 				print (visit.case,"form", visit.date_from, "to" ,visit.date_to)
-				if visit.date_to < search_date_from or visit.date_from > obj_visit.date_to:
-					pass
-				elif ( (visit.date_from <= search_date_from and visit.date_to >= search_date_from) or 
+				if ( (visit.date_from <= search_date_from and visit.date_to >= search_date_from) or 
 					(visit.date_from <= search_date_to and visit.date_to >= search_date_to) or 
 					(visit.date_from >= search_date_from and visit.date_to <= search_date_to)):
 					connections.append(visit)
 		print(connections)
-
-
-
-	context = {'selected_case':obj_case,'selected_visit':obj_visit, 'form':form, 'conn':connections}
-	template_name = "case/connections.html"
+		context = {'selected_case':obj_case,'selected_visit':obj_visit, 'form':form, 'conn':connections}
 	return render(request, template_name, context)
